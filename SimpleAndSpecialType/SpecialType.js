748df0f5a77541e471360b4db16c08f88b7f350e
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-12-09 21:40:41 +0530

    [Prajakta]Added: Simple and Special Type

---
+++ SimpleAndSpecialType/SpecialType.js
@@ -0,0 +1,17 @@
+//SimpleType
+//Explicit type:
+var firstName = "Dylan"; // type string
+console.log(typeof firstName);
+//Implicit Type:
+var lastName = "Dylan";
+console.log(typeof lastName);
+//SpecialType
+//any datatype
+var data = 10;
+data = "Komal";
+var data1 = [10, 'Pra', true];
+//undefined & null
+var y = undefined; //return undefine
+console.log(typeof y);
+var z = null; //return object
+console.log(typeof z);
---
+++ SimpleAndSpecialType/SpecialType.ts
@@ -0,0 +1,25 @@
+//SimpleType
+
+//Explicit type:
+let firstName: string = "Dylan"; // type string
+console.log(typeof firstName);
+
+//Implicit Type:
+let lastName = "Dylan";
+console.log(typeof lastName);
+
+//SpecialType
+
+//any datatype
+
+let data:any=10;
+data="Komal";
+
+let data1:any[]=[10,'Pra',true];
+
+//undefined & null
+let y: undefined = undefined;    //return undefine
+console.log(typeof y);
+
+let z: null = null;           //return object
+console.log(typeof z);
